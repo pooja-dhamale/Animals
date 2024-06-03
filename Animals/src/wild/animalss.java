d09006230927aa30c78ee56dd518c1cccb14474c
pdhama01 <pdhama01@192.168.2.5>
2024-06-03 16:56:40 +0530

    animals project

---
+++ Animals/src/wild/animalss.java
@@ -0,0 +1,12 @@
+package wild;
+
+public class animalss {
+
+	public static void main(String[]args) {
+		System.out.println("tiger");
+		System.out.println("Lion");
+		System.out.print("Deer");
+		System.out.println("Dog");
+	}
+	
+}
